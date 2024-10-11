0608cbef85e49593e456ea9d1fd6fafeaed6dd63
Isadora <132410644+isasilvino@users.noreply.github.com>
2024-10-11 11:30:44 -0300

    Update Aluno.java
    Estudo de POO (Programação Orientada a Objetos).
    
    Exercício com o intuito de mostrar o nome, matrícula e média do aluno. Usuário digita suas 3 notas e é feito o calculo da média.

--- Aluno/Aluno.java
+++ Aluno/Aluno.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package aluno;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author Aluno
- */
+
 public class Aluno {
 
     private String nome;
