b2e4e58556bdfc1fad365ed615c9b30e03e694b6
Isadora <132410644+isasilvino@users.noreply.github.com>
2024-10-11 11:23:41 -0300

    Update and rename Livro.java to Livro.java

--- Livro/Livro.java
+++ Livro/Livro.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package livro;
 
-/**
- *
- * @author Aluno
- */
+
 public class Livro {
 
     private String titulo;
