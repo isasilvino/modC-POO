2e3ef825162e0409ad7c899fd4cb830f8bd840d2
Isadora <132410644+isasilvino@users.noreply.github.com>
2024-10-11 11:32:47 -0300

    Update Main.java

--- Aluno/Main.java
+++ Aluno/Main.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package aluno;
 
 
@@ -10,6 +7,9 @@ public class Main {
     public static void main(String[] args) {
 
         Aluno aluno = new Aluno("Jorge", 2676);
+
+        System.out.println("Nome: "+ aluno.getNome());
+        System.out.println("Matricula: "+ aluno.getMatricula());
 
         aluno.calcularMedia();
 
