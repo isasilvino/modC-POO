0ff2e734d6f0a921e417010211736660352ca0ae
Isadora <132410644+isasilvino@users.noreply.github.com>
2024-10-11 11:23:15 -0300

    Update and rename Retangulo.java to Retangulo.java

--- Retângulo/Retangulo.java
+++ Retângulo/Retangulo.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package retangulo;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Aluno
- */
+
 public class Retangulo {
 
     private int altura;
