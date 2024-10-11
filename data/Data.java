808ef850e632f51207f401926531e2ca6000f347
Isadora <132410644+isasilvino@users.noreply.github.com>
2024-10-10 21:47:39 -0300

    Add files via upload

---
+++ data/Data.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package data;
+
+import java.text.SimpleDateFormat;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Data {
+
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public Data(int dia, int mes, int ano) {
+
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String formatarData() {
+
+        return String.format("%02d/%02d/%04d", this.dia, this.mes, this.ano);
+        
+
+        
+    }
+    public void data(){
+        
+        System.out.println(formatarData());
+    }
+
+}
---
+++ data/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package data;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        Data data = new Data(12, 04, 2013);
+        
+        data.data();
+    }
+    
+}
