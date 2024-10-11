e686c02e9a0bf68111e593f86a244cf5fb0f141b
Isadora <132410644+isasilvino@users.noreply.github.com>
2024-10-11 11:21:04 -0300

    Update and rename Main.java to Main.java

---
+++ Formatar Data/Main.java
@@ -0,0 +1,13 @@
+
+package data;
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Data data = new Data(12, 04, 2013);
+        
+        data.data();
+    }
+    
+}
